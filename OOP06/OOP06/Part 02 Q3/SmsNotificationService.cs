e4cb3dc6ad2ea594df4cf6344bd35cbb6d24157a
Mahmoud Fawzi <m.123fawzy@gmail.com>
2025-01-23 17:19:09 +0200

    OOP06

---
+++ OOP06/OOP06/Part 02 Q3/SmsNotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Part_02_Q3
+{
+    internal class SmsNotificationService : INotificationService
+    {
+        public void SendNotification(string recipient, string message)
+        {
+            Console.WriteLine($"Hello {recipient} , {message}");
+        }
+    }
+}
